329f57ea3293f4cabe301f04bfb54128b74a5dbb
Sabeur Rayan <85495356+RayanSabeur@users.noreply.github.com>
2024-09-11 14:51:57 +0200

    calculator

---
+++ src/Tests/CalculTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use App\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAddition()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+}
